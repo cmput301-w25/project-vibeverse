76c7a9627fda45fa03c17fc01a2bce6aacbfb501
saymanq <87442765+saymanq@users.noreply.github.com>
2025-02-25 17:07:40 -0700

    Added logout option on main page

--- code/app/src/main/java/com/example/vibeverse/Register.java
+++ code/app/src/main/java/com/example/vibeverse/Register.java
@@ -91,6 +91,9 @@ public class Register extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Register.this, "Account created successfully",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(Register.this, "Authentication failed.",
@@ -99,6 +102,6 @@ public class Register extends AppCompatActivity {
                             }
                         });
             }
-        })
+        });
     }
 }
