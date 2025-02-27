a235aeb2e52078c1ea72045d5d851f5517a56a61
tanmay-satija <157076642+tanmay-satija@users.noreply.github.com>
2025-02-27 08:21:02 -0700

    Updated

--- code/app/src/main/java/com/example/vibeverse/MainActivity.java
+++ code/app/src/main/java/com/example/vibeverse/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile); // Update if using another layout
+        setContentView(R.layout.activity_profile);
 
         // Initialize Bottom Navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
