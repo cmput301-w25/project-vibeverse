5cb3b4805c728c2155f3dc72f17a0b28a5e5d495
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2025-03-23 05:02:39 -0600

    Runs on Local with xml changes

---
+++ code/app/src/main/java/com/example/vibeverse/InsightsActivity.java
@@ -0,0 +1,12 @@
+package com.example.vibeverse;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsightsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mood_insight_main);
+    }
+}
