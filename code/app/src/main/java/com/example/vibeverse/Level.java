8683da160d065364fac8f1c01b02edc725789676
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2025-03-31 13:02:28 -0600

    Added javadoc comments for level files

--- code/app/src/main/java/com/example/vibeverse/Level.java
+++ code/app/src/main/java/com/example/vibeverse/Level.java
@@ -1,30 +1,64 @@
 package com.example.vibeverse;
 
+/**
+ * Represents a level in the application, containing the level number, the required XP to reach this level,
+ * and information about what is unlocked upon reaching this level.
+ */
 public class Level {
     private int level;
     private int xpRequired;
     private String unlocks; // <-- new field
 
+    /**
+     * Returns the level number.
+     *
+     * @return the level number.
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Sets the level number.
+     *
+     * @param level the level number to set.
+     */
     public void setLevel(int level) {
         this.level = level;
     }
 
+    /**
+     * Returns the XP required to reach this level.
+     *
+     * @return the required XP.
+     */
     public int getXpRequired() {
         return xpRequired;
     }
 
+    /**
+     * Sets the XP required to reach this level.
+     *
+     * @param xpRequired the required XP to set.
+     */
     public void setXpRequired(int xpRequired) {
         this.xpRequired = xpRequired;
     }
 
+    /**
+     * Returns the unlock information for this level.
+     *
+     * @return a String representing the unlocked feature or item, or "N/A" if none.
+     */
     public String getUnlocks() {
         return unlocks;
     }
 
+    /**
+     * Sets the unlock information for this level.
+     *
+     * @param unlocks the unlock information to set.
+     */
     public void setUnlocks(String unlocks) {
         this.unlocks = unlocks;
     }
--- code/app/src/main/java/com/example/vibeverse/LevelsWrapper.java
+++ code/app/src/main/java/com/example/vibeverse/LevelsWrapper.java
@@ -2,13 +2,29 @@ package com.example.vibeverse;
 
 import java.util.List;
 
+/**
+ * Wrapper class for a list of Level objects.
+ * <p>
+ * This class is used to encapsulate a collection of Level objects, typically for JSON deserialization.
+ * </p>
+ */
 public class LevelsWrapper {
     private List<Level> levels;
 
+    /**
+     * Returns the list of Level objects.
+     *
+     * @return the list of levels.
+     */
     public List<Level> getLevels() {
         return levels;
     }
 
+    /**
+     * Sets the list of Level objects.
+     *
+     * @param levels the list of levels to set.
+     */
     public void setLevels(List<Level> levels) {
         this.levels = levels;
     }
